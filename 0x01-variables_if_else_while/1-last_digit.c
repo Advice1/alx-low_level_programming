7cd3b200731d8c7604719151d1325b9a3d8a2b5c
Advice <drnxumza@gmail.com>
2022-07-03 07:32:31 -0700

    adding an if statement to my code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@ int lastdigit;
 
 srand(time(0));  
 n = rand() - RAND_MAX / 2;
- lastdigit = n % 10; /*last digit of my random number*/
+lastdigit = n % 10; /*last digit of my random number*/
 
 if(lastdigit>5)
 {
@@ -20,7 +20,7 @@ else if(lastdigit==0)
 {
 printf("Last digit of %d is %d and is 0\n",n,lastdigit);
 }
-else if(lastdigit<6 && lastdigit>0)
+else if(lastdigit<6)
 {     
 printf("Last digit of %d is %d and is less than 6 and not 0\n",n,lastdigit);
 }
